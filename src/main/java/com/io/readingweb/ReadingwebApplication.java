b3fb50fdefd7a76ac29b156e678501762726d242
HungLeManh <113190663+HungLeManh@users.noreply.github.com>
2025-08-28 11:03:50 +0700

    Init commit

---
+++ src/main/java/com/io/readingweb/ReadingwebApplication.java
@@ -0,0 +1,13 @@
+package com.io.readingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/readingweb/ReadingwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.readingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
